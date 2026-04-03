ffc6ab1e4a8d2161d7c0242e2f14591adc7f955c
Ori <orip23@gmail.com>
2026-04-03 15:05:20 +0300

    move main to presentation1

--- dev/presentation/Main.java
+++ dev/presentation/Main.java
@@ -3,6 +3,6 @@ package dev.presentation;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello! The system is running.");
+        System.out.println("Hello! The system is running.a");
     }
 }
