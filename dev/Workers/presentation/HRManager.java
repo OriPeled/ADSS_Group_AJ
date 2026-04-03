e44131ffd6829755547b4ebdfc950f157e1ab135
Ori <orip23@gmail.com>
2026-04-03 19:29:45 +0300

    add pack workers 223

--- dev/Workers/presentation/HRManager.java
+++ dev/Workers/presentation/HRManager.java
@@ -7,6 +7,7 @@ import dev.Workers.domain.Shift;
 import java.util.*;
 
 public class HRManager {
+
     private Map<Shift, Map<Role, List<Employee>>> shifts;
     private List<Role> availableRoles;
     //private Map<Role, Integer> requirements;
