d576cefba8e6e50d6b101aca0e87d4f6ace87784
Ori Peled <155752515+OriPeled@users.noreply.github.com>
2026-06-24 13:35:21 +0300

    edit the ui, earase E2E

--- dev/Workers/presentation/ManageShiftsMenu.java
+++ dev/Workers/presentation/ManageShiftsMenu.java
@@ -416,7 +416,7 @@ public class ManageShiftsMenu {
                         return;
                     }
                 } catch (Exception ex) {
-                    System.out.println(ex.getMessage());
+                   // System.out.println(ex.getMessage());
                 }
             }
         }
